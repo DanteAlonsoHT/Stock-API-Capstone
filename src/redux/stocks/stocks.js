e124d3a0662dd14ba01acf61d5f76ef3f7b22777
DanteAlonsoHT <dante1818@hotmail.com>
2021-10-16 01:40:21 -0500

    Enable API consumption instead of mockup

--- src/redux/stocks/stocks.js
+++ src/redux/stocks/stocks.js
@@ -1,10 +1,9 @@
-/* eslint-disable camelcase */
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-// import axios from 'axios';
+import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 
-// const API_KEY = '819c2b8705e5c67cf963a89ad94aa274';
-// const STOCKS_API_URL = 'https://financialmodelingprep.com/api/v3/available-traded/list?apikey=' + API_KEY;
+const API_KEY = '819c2b8705e5c67cf963a89ad94aa274';
+const STOCKS_API_URL = `https://financialmodelingprep.com/api/v3/available-traded/list?apikey=${API_KEY}`;
 
 const initialState = {
   isFetching: false,
@@ -15,98 +14,13 @@ const initialState = {
 export const getStocks = createAsyncThunk(
   'redux/stocks/stocks.js',
   async () => {
-    // const response = await axios.get(STOCKS_API_URL).catch((error) => error);
-    const response = [{
-      symbol: 'SPY',
-      name: 'SPDR S&P 500 ETF Trust',
-      price: 435.21,
-      exchange: 'New York Stock Exchange Arca',
-      exchangeShortName: 'AMEX',
-      type: 'etf',
-    }, {
-      symbol: 'CMCSA',
-      name: 'Comcast Corporation',
-      price: 52.69,
-      exchange: 'Nasdaq Global Select',
-      exchangeShortName: 'NASDAQ',
-      type: 'stock',
-    }, {
-      symbol: 'KMI',
-      name: 'Kinder Morgan, Inc.',
-      price: 17.97,
-      exchange: 'New York Stock Exchange',
-      exchangeShortName: 'NYSE',
-      type: 'stock',
-    }, {
-      symbol: 'INTC',
-      name: 'Intel Corporation',
-      price: 52.26,
-      exchange: 'Nasdaq Global Select',
-      exchangeShortName: 'NASDAQ',
-      type: 'stock',
-    }, {
-      symbol: 'MU',
-      name: 'Micron Technology, Inc.',
-      price: 66.38,
-      exchange: 'Nasdaq Global Select',
-      exchangeShortName: 'NASDAQ',
-      type: 'stock',
-    }, {
-      symbol: 'GDX',
-      name: 'VanEck Vectors Gold Miners ETF',
-      price: 32.19,
-      exchange: 'New York Stock Exchange Arca',
-      exchangeShortName: 'AMEX',
-      type: 'etf',
-    }, {
-      symbol: 'GE',
-      name: 'General Electric Company',
-      price: 102.36,
-      exchange: 'New York Stock Exchange',
-      exchangeShortName: 'NYSE',
-      type: 'stock',
-    }, {
-      symbol: 'BAC',
-      name: 'Bank of America Corporation',
-      price: 43.14,
-      exchange: 'New York Stock Exchange',
-      exchangeShortName: 'NYSE',
-      type: 'stock',
-    }, {
-      symbol: 'EEM',
-      name: 'iShares MSCI Emerging Markets ETF',
-      price: 51.16,
-      exchange: 'New York Stock Exchange Arca',
-      exchangeShortName: 'AMEX',
-      type: 'etf',
-    }, {
-      symbol: 'XLF',
-      name: 'Financial Select Sector SPDR Fund',
-      price: 38.27,
-      exchange: 'New York Stock Exchange Arca',
-      exchangeShortName: 'AMEX',
-      type: 'etf',
-    }, {
-      symbol: 'AAPL',
-      name: 'Apple Inc.',
-      price: 140.91,
-      exchange: 'Nasdaq Global Select',
-      exchangeShortName: 'NASDAQ',
-      type: 'stock',
-    }, {
-      symbol: 'MSFT',
-      name: 'Microsoft Corporation',
-      price: 296.31,
-      exchange: 'Nasdaq Global Select',
-      exchangeShortName: 'NASDAQ',
-      type: 'stock',
-    }];
+    const response = await axios.get(STOCKS_API_URL).catch((error) => error);
     const data = [];
 
-    response.forEach((obj) => {
+    for (let i = 0; i < 51; i += 1) {
       const {
         symbol, name, price, exchange, exchangeShortName,
-      } = obj;
+      } = response.data[i];
 
       const formatedData = {
         stockId: uuidv4(),
@@ -119,7 +33,7 @@ export const getStocks = createAsyncThunk(
       };
 
       data.push(formatedData);
-    });
+    }
 
     return data;
   },

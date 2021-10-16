b28d09bd072e5fe7232abec2bdc90895d678228a
DanteAlonsoHT <dante1818@hotmail.com>
2021-10-16 03:13:38 -0500

    Add tests for API consumption using Jest

---
+++ src/redux/stocks/stocks.mock.test.js
@@ -0,0 +1,20 @@
+/* eslint no-return-await: "error" */
+import axios from 'axios';
+
+const API_KEY = '819c2b8705e5c67cf963a89ad94aa274';
+export const STOCKS_API_URL = `https://financialmodelingprep.com/api/v3/available-traded/list?apikey=${API_KEY}`;
+
+export const fetchData = async () => {
+  const url = STOCKS_API_URL;
+
+  return await axios.get(url);
+};
+
+fetchData('react');
+
+describe('Testing API axios', () => {
+  test('URL is the correct', () => {
+    const url = 'https://financialmodelingprep.com/api/v3/available-traded/list?apikey=819c2b8705e5c67cf963a89ad94aa274';
+    expect(url).toStrictEqual(STOCKS_API_URL);
+  });
+});
